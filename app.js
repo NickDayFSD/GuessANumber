6dffbe9e028926d7db54b29da3570a12b166e6a1
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-24 16:15:24 -0700

    added a reset button that resets the random number and guesses left

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ import { isNumber } from './utils.js';
 const userNumber = document.getElementById('user-guess');
 const results = document.getElementById('results');
 const guessNow = document.getElementById('guess-now');
+const tryAgain = document.getElementById('try-again');
 
 // initialize state
 let randomNumber = Math.ceil(Math.random() * 20);
-let guessesLeft = 3;
+let guessesLeft = 4;
 //console.log(userNumber.value, 'value of my input');
 
 console.log(randomNumber);
@@ -21,10 +22,17 @@ guessNow.addEventListener('click', () => {
     guessesLeft--;
 
     console.log(guessesLeft);
-    console.log(userNumber.value, 'value of input');
+    //console.log(userNumber.value, 'value of input');
 
     const guessResults = isNumber(userNumber.value, randomNumber);
     console.log(guessResults, 'guesses left');
     results.textContent = guessResults;
     return `You have ${guessesLeft} guesses remaining.`;
 });
+
+tryAgain.addEventListener('click', () => {
+    randomNumber = Math.ceil(Math.random() * 20);
+    guessesLeft = 4;
+    console.log(randomNumber, 'random number reset');
+    console.log(guessesLeft, 'guesses reset')
+})
