67e3f671caf9865f3eafb77bff4654038470f60c
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-03-26 13:39:01 -0700

    fixed github errors

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ let randomNumber = Math.ceil(Math.random() * 20);
 let guessesLeft = 4;
 //console.log(userNumber.value, 'value of my input');
 guessNow.classList.remove('hidden');
-
-console.log(randomNumber);
 // set event listeners to update state and DOM
 guessNow.addEventListener('click', () => {
-    if (guessesLeft <= 0){
+    if (guessesLeft <= 0) {
         alert('You ran out of guesses!');
         return;
     }
@@ -29,7 +27,7 @@ guessNow.addEventListener('click', () => {
         results.textContent = guessResults;
         guessNow.classList.add('hidden');
         tryAgain.classList.remove('hidden');
-    };
+    }
     //if user wins hide guess button and reveal reset button
     //if user doesn't win, check results
     results.textContent = `${guessResults} You have ${guessesLeft} guesses remaining.`;
@@ -40,8 +38,6 @@ guessNow.addEventListener('click', () => {
 tryAgain.addEventListener('click', () => {
     randomNumber = Math.ceil(Math.random() * 20);
     guessesLeft = 4;
-    console.log(randomNumber, 'random number reset');
-    console.log(guessesLeft, 'guesses reset');
     userNumber.value = '';
     results.textContent = '';
     guessNow.classList.toggle('hidden');
